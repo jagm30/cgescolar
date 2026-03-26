e39394e3b0d00e4c6cce7bb4cc088e5a097a3e50
EduardoNucamendi <eduardonuve220@gmail.com>
2026-03-26 12:58:39 -0600

    Vistas componentes

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/tables', function () {return view('tables');})->name('tables');
-Route::get('/datatables', function () {return view('data');})->name('datatables');
-Route::get('/ui-elements', function () {return view('uiGeneral');})->name('ui-elements');
-Route::get('/forms', function () {return view('forms');})->name('forms');
-Route::get('/icons', function () {return view('icons');})->name('icons');
-Route::get('/widgets', function () {return view('widgets');})->name('widgets');
+Route::get('/tables', function () {return view('plantilla.tables');})->name('tables');
+Route::get('/datatables', function () {return view('plantilla.data');})->name('datatables');
+Route::get('/ui-elements', function () {return view('plantilla.uiGeneral');})->name('ui-elements');
+Route::get('/forms', function () {return view('plantilla.forms');})->name('forms');
+Route::get('/icons', function () {return view('plantilla.icons');})->name('icons');
+Route::get('/widgets', function () {return view('plantilla.widgets');})->name('widgets');
 // =======================================================
 // Rutas públicas — sin autenticación
 // =======================================================
