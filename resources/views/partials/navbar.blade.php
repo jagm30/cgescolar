e07bad1c98083a3f3832015145d99404879b1c82
SamayoaChamba <L20270788@tuxtla.tecnm.mx>
2026-03-26 13:12:52 -0600

    Corrigiendo nombre de usuario que no aparecia en el modal de imagen

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -16,16 +16,16 @@
 
                     <li class="dropdown user user-menu">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
+                            <img src="{{ asset('dist/img/avatar5.png') }}" class="user-image" alt="User Image">
                             <span class="hidden-xs">{{ auth()->user()->name }}</span>
                         </a>
                         <ul class="dropdown-menu">
                             <li class="user-header">
-                                <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle"
-                                    alt="User Image">
+                                <img src="{{ asset('dist/img/avatar5.png') }}" class="img-circle" alt="User Image">
                                 <p>
-                                    {{ auth()->user()->name }} - {{ auth()->user()->rol }}
-                                    <small>Activo desde 2026</small>
+                                    {{ auth()->user()->nombre }}
+                                    <small>{{ auth()->user()->email }}</small>
+
                                 </p>
                             </li>
                             <li class="user-footer">
